198c87df6fd254c9b8d067836f16e1202edd4bbc
takahashi-akira <takahashi-akira@KMS2013-68.kms.local>
2018-06-07 13:56:01 +0900

    エラーページに飛ぶように変更

--- src/servlet/DirectMessageServlet.java
+++ src/servlet/DirectMessageServlet.java
@@ -28,27 +28,28 @@ public class DirectMessageServlet extends HttpServlet {
 		HttpSession session = req.getSession();
 		if (session.getAttribute("session") == null) {
 			req.getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, res);
-		}
-		//自会員番号を取得
-		LoginBean loginBean = (LoginBean) session.getAttribute("loginBean");
-		String myLogin = loginBean.getUserNo();
-		//相手の会員番号を取得
-		bean.setToUserNo(Integer.parseInt(req.getParameter("toUserNo")));
-		Integer toUserNo = (bean.getToUserNo());
-		// 会話情報の取得
-		try {
-			bean = model.authentication(bean, loginBean);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// もしも相手の番号が無い場合はエラーを表示
-		if (toUserNo == 0) {
-			req.getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, res);
 		} else {
-			req.setAttribute("messageCheckBean", bean);
-			req.setAttribute("myLoginNo", myLogin);
-			session.setAttribute("messageCheckBean", bean); //セッション内へ自分と相手の情報を保存
-			req.getRequestDispatcher("/WEB-INF/jsp/directMessage.jsp").forward(req, res);
+			//自会員番号を取得
+			LoginBean loginBean = (LoginBean) session.getAttribute("loginBean");
+			String myLogin = loginBean.getUserNo();
+			//相手の会員番号を取得
+			bean.setToUserNo(Integer.parseInt(req.getParameter("toUserNo")));
+			Integer toUserNo = (bean.getToUserNo());
+			// 会話情報の取得
+			try {
+				bean = model.authentication(bean, loginBean);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			// もしも相手の番号が無い場合はエラーを表示
+			if (toUserNo == 0) {
+				req.getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, res);
+			} else {
+				req.setAttribute("messageCheckBean", bean);
+				req.setAttribute("myLoginNo", myLogin);
+				session.setAttribute("messageCheckBean", bean); //セッション内へ自分と相手の情報を保存
+				req.getRequestDispatcher("/WEB-INF/jsp/directMessage.jsp").forward(req, res);
+			}
 		}
 	}
 
--- src/servlet/GroupMessageServlet.java
+++ src/servlet/GroupMessageServlet.java
@@ -32,27 +32,28 @@ public class GroupMessageServlet extends HttpServlet {
 		HttpSession session = req.getSession();
 		// ログインできているか確認
 		if (session.getAttribute("session") == null) {
-			req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, res);
-		}
-		// 自会員番号を取得
-		LoginBean loginBean = (LoginBean) session.getAttribute("loginBean");
-		// 相手の会員番号の取得
-		bean.setGroupNo((req.getParameter("toGroupNo")));
-		Mbean.setToUserNo(Integer.parseInt((req.getParameter("toGroupNo"))));
-		//String toGroupNo = (bean.getGroupNo());
-		// 会話情報の取得
-		try {
-			bean = model.authentication(bean, loginBean, req.getParameter("toGroupNo"));
-			Mbean = Mmodel.authentication(Mbean, loginBean);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			req.getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, res);
+		} else {
+			// 自会員番号を取得
+			LoginBean loginBean = (LoginBean) session.getAttribute("loginBean");
+			// 相手の会員番号の取得
+			bean.setGroupNo((req.getParameter("toGroupNo")));
+			Mbean.setToUserNo(Integer.parseInt((req.getParameter("toGroupNo"))));
+			//String toGroupNo = (bean.getGroupNo());
+			// 会話情報の取得
+			try {
+				bean = model.authentication(bean, loginBean, req.getParameter("toGroupNo"));
+				Mbean = Mmodel.authentication(Mbean, loginBean);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-		// もしも相手の番号が無い場合はエラーを表示
-		req.setAttribute("GroupBean", bean);
-		req.setAttribute("myLoginNo", loginBean.getUserNo());
-		session.setAttribute("GroupBean", Mbean); //セッション内へ自分と相手の情報を保存
-		req.getRequestDispatcher("/WEB-INF/jsp/groupMessage.jsp").forward(req, res);
+			// もしも相手の番号が無い場合はエラーを表示
+			req.setAttribute("GroupBean", bean);
+			req.setAttribute("myLoginNo", loginBean.getUserNo());
+			session.setAttribute("GroupBean", Mbean); //セッション内へ自分と相手の情報を保存
+			req.getRequestDispatcher("/WEB-INF/jsp/groupMessage.jsp").forward(req, res);
+		}
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -64,36 +65,37 @@ public class GroupMessageServlet extends HttpServlet {
 		 * */
 		HttpSession session = req.getSession();
 		if (session.getAttribute("session") == null) {
-			req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, res);
-		}
-		// 現在のセッションに入っているmessageCheckBean情報を受け取る
-		MessageCheckBean bean = (MessageCheckBean) session.getAttribute("GroupBean");
-		MessageCheckSendModel model = new MessageCheckSendModel();
-		//現在のセッションに入っているloginBean情報を受け取る
-		LoginBean loginBean = (LoginBean) session.getAttribute("loginBean");
-		//メッセージ内容を取得
-		String sendMessage = new String(req.getParameter("sendMessage").getBytes("ISO-8859-1"));
-		bean.setSendMessage(sendMessage);
-		//String sendMessage = bean.getSendMessage();
-
-		//入力チェックの返答
-		int bytecheck = 0;
-		bytecheck = bean.stringLengthCheck(sendMessage);
-		if (bytecheck == 1) {
-			req.setAttribute("error","文字数オーバーです");
-			doGet(req, res);
+			req.getRequestDispatcher("/WEB-INF/jsp/errorPage.jsp").forward(req, res);
 		} else {
-			// 会話情報の取得
-			try {
-				model.sendGroupMessage(bean, loginBean);
-				//				bean = model.authentication(bean, loginBean);
-				//最新情報が表示されていないため、情報更新用処理。
-				//前回情報と合わせて表示されてしまっているためコメントアウト中
-			} catch (Exception e) {
-				e.printStackTrace();
+			// 現在のセッションに入っているmessageCheckBean情報を受け取る
+			MessageCheckBean bean = (MessageCheckBean) session.getAttribute("GroupBean");
+			MessageCheckSendModel model = new MessageCheckSendModel();
+			//現在のセッションに入っているloginBean情報を受け取る
+			LoginBean loginBean = (LoginBean) session.getAttribute("loginBean");
+			//メッセージ内容を取得
+			String sendMessage = new String(req.getParameter("sendMessage").getBytes("ISO-8859-1"));
+			bean.setSendMessage(sendMessage);
+			//String sendMessage = bean.getSendMessage();
+
+			//入力チェックの返答
+			int bytecheck = 0;
+			bytecheck = bean.stringLengthCheck(sendMessage);
+			if (bytecheck == 1) {
+				req.setAttribute("error", "文字数オーバーです");
+				doGet(req, res);
+			} else {
+				// 会話情報の取得
+				try {
+					model.sendGroupMessage(bean, loginBean);
+					//				bean = model.authentication(bean, loginBean);
+					//最新情報が表示されていないため、情報更新用処理。
+					//前回情報と合わせて表示されてしまっているためコメントアウト中
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				doGet(req, res);
+				//			req.getRequestDispatcher("/WEB-INF/jsp/directMessage.jsp").forward(req, res);
 			}
-			doGet(req, res);
-			//			req.getRequestDispatcher("/WEB-INF/jsp/directMessage.jsp").forward(req, res);
 		}
 	}
 }
