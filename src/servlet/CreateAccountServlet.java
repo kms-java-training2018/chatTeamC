ce3cd59238b38e27dd80b80e2d814da8e44e4a41
onishi-yasumasa <onishi-yasumasa@KMS2013-110.kms.local>
2018-06-21 15:40:53 +0900

    修正

--- src/servlet/CreateAccountServlet.java
+++ src/servlet/CreateAccountServlet.java
@@ -36,36 +36,34 @@ public class CreateAccountServlet extends HttpServlet {
 		CreateAccount model = new CreateAccount();
 		String direction = "/WEB-INF/jsp/login.jsp";
 
-
 		// パラメータの取得
 		bean.setUserId((String) req.getParameter("userId"));
 		bean.setPassword((String) req.getParameter("password"));
 		bean.setUserName((String) req.getParameter("userName"));
 		bean.setProfile((String) req.getParameter("profile"));
 
-
 		//文字入力チェック
 		bean = model.checkData(bean);
 		//OKだったら登録、Noだったらエラー
-		if(bean.isCheckJudge() == true) {
+		if (bean.isCheckJudge() == true) {
 
-		try {
+			try {
 				// 認証処理
 				bean = model.resistUser(bean);
 
+			} catch (Exception e) {
+				direction = "/WEB-INF/jsp/createAccount.jsp";
+				bean.setErrorMessage("登録できませんでした。");
+			}
 
-		} catch (Exception e) {
+			// 取得に成功した場合セッション情報をセット
+			if ("".equals(bean.getErrorMessage())) {
+				bean.setErrorMessage("会員登録出来ました。");
+
+			}
+		} else {
 			direction = "/WEB-INF/jsp/createAccount.jsp";
-			bean.setErrorMessage("登録できませんでした。");
-		}
-
-		// 取得に成功した場合セッション情報をセット
-		if ("".equals(bean.getErrorMessage())) {
-			bean.setErrorMessage("会員登録出来ました。");
-
-		}
-		}else {
-		req.setAttribute("loginError", bean);
+			req.setAttribute("loginError", bean);
 		}
 
 		req.getRequestDispatcher(direction).forward(req, res);
